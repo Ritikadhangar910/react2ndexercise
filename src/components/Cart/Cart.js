7f1f4e8b50533a3fcd3ca2331c166ab79c16b37d
dhangarritika <dhangarritika90@gmail.com>
2023-09-05 13:22:25 +0530

    impliment portals

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,35 @@
+import Model from "../UI/Model";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const CartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "c1",
+          name: "sushi",
+          amount: 2,
+          price: 12.65,
+        },
+      ].map((item) => {
+        return <li>{item.name}</li>;
+      })}
+    </ul>
+  );
+  return (
+    <>
+      <Model>
+        {CartItems}
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>35.62</span>
+        </div>
+        <div className={classes.actions}>
+          <button className={classes["button--alt"]}>Close</button>
+          <button className={classes.button}>Order</button>
+        </div>
+      </Model>
+    </>
+  );
+};
+export default Cart;
---
+++ src/components/UI/Model.js
@@ -0,0 +1,22 @@
+import ReactDOM from "react-dom";
+import classes from "./Model.module.css";
+const Backdrop = () => {
+  return <div className={classes.backdrop} />;
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Model = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(Backdrop, portalElement)}
+      {ReactDOM.createPortal(
+        <div className={classes.modal}>
+          <div className={classes.content}>{props.children}</div>
+        </div>,
+        portalElement
+      )}
+    </>
+  );
+};
+export default Model;
