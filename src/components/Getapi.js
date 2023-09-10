2eef9398fd59849494afe3e48fd3f103e947670b
dhangarritika <dhangarritika90@gmail.com>
2023-09-10 20:23:25 +0530

    setinterval

--- src/components/Getapi.js
+++ src/components/Getapi.js
@@ -1,30 +1,79 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+
 const Getapi = () => {
-  const [items, setitem] = useState([]);
-  const [loader, setloader] = useState(false);
-  async function callapi() {
-    setloader(true);
-    let apis = await fetch("https://dummyjson.com/products");
-    apis = await apis.json();
-    setloader(false);
-    setitem(apis);
-  }
+  const [items, setItems] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+  const [retrying, setRetrying] = useState(false); // Indicates whether retrying is in progress
+  const [retryInterval, setRetryInterval] = useState(null); // Interval ID for retrying
+
+  const callApi = async () => {
+    try {
+      setLoader(true);
+      let apis = await fetch("https://jsonplaceholder.typicode.com/post");
+      if (!apis.ok) {
+        throw new Error("Data is not fetching");
+      }
+      apis = await apis.json();
+      setItems(apis);
+      setError(false);
+    } catch (err) {
+      setError(err);
+      setRetrying(true); // Set retrying to true on failure
+    }
+    setLoader(false);
+  };
+
   useEffect(() => {
-    callapi();
+    callApi();
   }, []);
-  console.log(items);
+
+  // Retry function that will be called at a 5-second interval
+  const retry = () => {
+    if (!retrying) return; // Stop retrying if retrying is set to false
+    callApi(); // Retry the API call
+  };
+
+  // Start retrying with a 5-second interval
+  useEffect(() => {
+    if (retrying) {
+      const intervalId = setInterval(retry, 2000);
+      setRetryInterval(intervalId);
+
+      // Cleanup the interval when the component unmounts or when retrying is canceled
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [retrying]);
+
+  // Function to cancel retrying
+  const cancelRetry = () => {
+    setRetrying(false); // Stop retrying
+    clearInterval(retryInterval); // Clear the retry interval
+  };
+
   return (
     <>
-      <h1>this is fun</h1>
-      {loader && <p>loader..</p>}
+      <h1>This is fun</h1>
+      {loader && <p>Loading...</p>}
       {items.length > 0 &&
+        !error &&
         items.map((item) => (
           <div key={item.id}>
-            <p>mybody:{item.title}</p>
+            <p>User ID: {item.userId}</p>
+            <p>Title: {item.title}</p>
           </div>
         ))}
-      {!Array.isArray(items) && <p>data is not fetching</p>}
+      {error && <p>Data is not fetching</p>}
+      {retrying && (
+        <>
+          <p>Retrying in 5 seconds...</p>
+          <button onClick={cancelRetry}>Cancel Retry</button>
+        </>
+      )}
     </>
   );
 };
+
 export default Getapi;
